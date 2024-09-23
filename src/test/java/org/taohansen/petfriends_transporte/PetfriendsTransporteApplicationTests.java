c9787d07c61fe4b7f58d2a9a5374535c8824013b
Tao Hansen <53713149+taohansens@users.noreply.github.com>
2024-09-22 23:01:31 -0300

    Init project

---
+++ src/test/java/org/taohansen/petfriends_transporte/PetfriendsTransporteApplicationTests.java
@@ -0,0 +1,13 @@
+package org.taohansen.petfriends_transporte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetfriendsTransporteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
